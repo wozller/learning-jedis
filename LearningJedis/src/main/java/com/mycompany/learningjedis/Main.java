b7cd30310428a2e96236fa23d306c4cdb8d2b595
wozller <brian.sanchez.wozller@gmail.com>
2022-07-26 13:40:48 -0400

    Set and get a key-value of strings

--- LearningJedis/src/main/java/com/mycompany/learningjedis/Main.java
+++ LearningJedis/src/main/java/com/mycompany/learningjedis/Main.java
@@ -11,6 +11,13 @@ public class Main {
         
         Jedis jedis = new Jedis();
         
+        // Setting a simple key-value of strings.
+        jedis.set("events/city/rome", "32,15,223,828");
+        
+        // Getting that key-value of strings.
+        String cachedResponse = jedis.get("events/city/rome");
+        System.out.println(cachedResponse);
+        
     } // End of main method.
     
 } // End of Main class.
