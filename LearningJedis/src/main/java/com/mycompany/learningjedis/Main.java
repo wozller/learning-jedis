6f30bbba643373af461230beb414e78ee063b4e8
wozller <brian.sanchez.wozller@gmail.com>
2022-07-26 11:21:36 -0400

    Create Java project with Main testing class
    Create a blank Java project with a main class. This is where Jedis will
    be experimented with (note that Jedis hasn't been set up yet).

---
+++ LearningJedis/src/main/java/com/mycompany/learningjedis/Main.java
@@ -0,0 +1,12 @@
+package com.mycompany.learningjedis;
+
+/**
+ * @author wozller
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    } // End of main method.
+    
+} // End of Main class.
